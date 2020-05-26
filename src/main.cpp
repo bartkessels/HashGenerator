3d198b0db4956b7665238b1e94cf0265a922c912
Bart Kessels <development@bk-mail.com>
2020-05-26 21:50:04 +0200

    Add icon for application

--- src/main.cpp
+++ src/main.cpp
@@ -6,9 +6,14 @@
 
 int main(int argc, char** argv)
 {
+    const auto& appName = "Hash Generator";
     const auto& factory = std::make_shared<hashgen::domain::HashFactory>();
 
     QApplication app(argc, argv);
+    QApplication::setWindowIcon(QIcon(":/icons/main"));
+    QApplication::setApplicationDisplayName(appName);
+    QApplication::setApplicationName(appName);
+
     hashgen::gui::MainWindow window(factory);
     window.show();
 
